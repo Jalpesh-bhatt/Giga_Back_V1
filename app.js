3254416aa02dedae0c3dd49423240d3daee192b7
Jalpesh-bhatt <129989844+Jalpesh-bhatt@users.noreply.github.com>
2025-11-26 11:25:26 +0530

    Add static file serving and catch-all route
    Added static file serving for the build directory and a catch-all route to serve index.html.

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static("build")); 
+app.get("*", (req, res) => {res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 
 // mongoose.connect('mongodb://localhost:27017/business', {
 
@@ -58,3 +62,4 @@ app.use('/api/admin/category', CategoryRoutes);
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+
